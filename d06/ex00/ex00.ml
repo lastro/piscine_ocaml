41673d754adb785d31338e9ce76f15c190420769
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-24 20:13:32 +0200

    ex00

---
+++ d06/ex00/ex00.ml
@@ -0,0 +1,18 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                                            *)
+(*   ex00.ml                                                                  *)
+(*                                                                            *)
+(*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
+(*                                                                            *)
+(*   Created: 2015/06/24 19:53:15 by tlepetit                                 *)
+(*   Updated: 2015/06/24 20:10:55 by tlepetit                                 *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+module StringSet = Set.Make(struct let compare = Pervasives.compare type t = string end)
+
+let () =
+  let set = List.fold_right StringSet.add [ "foo"; "bar"; "baz"; "qux" ] StringSet.empty in
+  StringSet.iter print_endline set;
+  print_endline (StringSet.fold ( ^ ) set "")
