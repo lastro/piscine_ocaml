466ee62418708134e637e9ae8b3a602f3af7503e
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-25 22:26:39 +0200

    correction equals d08/ex00

--- d08/ex00/atom.ml
+++ d08/ex00/atom.ml
@@ -6,14 +6,16 @@
 (*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
 (*                                                                            *)
 (*   Created: 2015/06/25 19:45:05 by tlepetit                                 *)
-(*   Updated: 2015/06/25 21:19:29 by tlepetit                                 *)
+(*   Updated: 2015/06/25 22:22:34 by tlepetit                                 *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 class virtual atom name symbol atomic_number =
-object
+object (self)
   method name = name
   method symbol = symbol
-  method atomic_number = atomic_number
+  method atomic_number =
+	atomic_number
   method to_string = "Atom: " ^ name ^ " Symbol: " ^ symbol ^ " Atomic number: " ^ (string_of_int atomic_number)
+  method equals (a:atom) = (name = a#name) && (symbol = a#symbol) && (atomic_number = a#atomic_number)
 end
--- d08/ex00/main.ml
+++ d08/ex00/main.ml
@@ -6,7 +6,7 @@
 (*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
 (*                                                                            *)
 (*   Created: 2015/06/25 19:45:17 by tlepetit                                 *)
-(*   Updated: 2015/06/25 21:49:20 by tlepetit                                 *)
+(*   Updated: 2015/06/25 22:26:05 by tlepetit                                 *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -16,5 +16,15 @@ let () =
   let o = new Oxygen.oxygen in
   print_endline o#to_string;
   let c = new Carbon.carbon in
-  print_endline c#to_string
+  print_endline c#to_string;
+  if not (c#equals o) then
+	print_endline "Carbon is not oxygen!"
+  else
+	print_endline "What?";
+  let h2 = new Hydrogen.hydrogen in
+  if h#equals h2 then
+	print_endline "Both hydrogens are equal."
+  else
+	print_endline "uh?"
+				  
 
