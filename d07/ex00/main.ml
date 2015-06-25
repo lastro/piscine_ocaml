7cc72289256bd9296c0ff9e388314c6c980683bf
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-25 16:43:19 +0200

    correction vie mutable d07/ex00

--- d07/ex00/main.ml
+++ d07/ex00/main.ml
@@ -2,4 +2,5 @@ let () =
   let arthur = new People.people "Arthur" in
   arthur#talk;
   print_endline "A dalek is here!";
-  arthur#die
+  arthur#die;
+  print_endline arthur#to_string
--- d07/ex00/people.ml
+++ d07/ex00/people.ml
@@ -1,10 +1,10 @@
 class people name =
 object (self)
   val _name:string = name
-  val _hp = 100
-  method to_string = "Instance of class people. Name: " ^ name ^ " hp: " ^ (string_of_int _hp)
+  val mutable _hp = 100
+  method to_string = "Instance of class people. Name: " ^ _name ^ " hp: " ^ (string_of_int _hp)
   method talk = print_endline ("I'm " ^ _name ^ "! Do you know the doctor?")
-  method die = print_endline "Aaaarghh!"
+  method die = print_endline "Aaaarghh!"; _hp <- 0
   initializer print_endline "A new character appears!"; print_endline self#to_string
 end
   
