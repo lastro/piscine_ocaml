58a448474ac5006b5fb9c084b1575fe8a2f04d3f
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-25 21:30:34 +0200

    d08/ex00

---
+++ d08/ex00/atom.ml
@@ -0,0 +1,19 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                                            *)
+(*   atom.ml                                                                  *)
+(*                                                                            *)
+(*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
+(*                                                                            *)
+(*   Created: 2015/06/25 19:45:05 by tlepetit                                 *)
+(*   Updated: 2015/06/25 21:19:29 by tlepetit                                 *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+class virtual atom name symbol atomic_number =
+object
+  method name = name
+  method symbol = symbol
+  method atomic_number = atomic_number
+  method to_string = "Atom: " ^ name ^ " Symbol: " ^ symbol ^ " Atomic number: " ^ (string_of_int atomic_number)
+end
