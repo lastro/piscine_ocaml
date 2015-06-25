fb9d157fcf79941740688ce9e32a3148ddea281a
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-25 21:49:56 +0200

    correction main d08/ex00

--- d08/ex00/main.ml
+++ d08/ex00/main.ml
@@ -6,12 +6,15 @@
 (*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
 (*                                                                            *)
 (*   Created: 2015/06/25 19:45:17 by tlepetit                                 *)
-(*   Updated: 2015/06/25 21:29:52 by tlepetit                                 *)
+(*   Updated: 2015/06/25 21:49:20 by tlepetit                                 *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let () =
   let h = new Hydrogen.hydrogen in
-  print_endline h#to_string
-
+  print_endline h#to_string;
+  let o = new Oxygen.oxygen in
+  print_endline o#to_string;
+  let c = new Carbon.carbon in
+  print_endline c#to_string
 
