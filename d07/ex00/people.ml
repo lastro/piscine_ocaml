c1ee71bf251175218484423556f0ad06b9174070
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-25 15:59:50 +0200

    correction d07/ex00:

--- d07/ex00/people.ml
+++ d07/ex00/people.ml
@@ -1,8 +1,8 @@
 class people name =
 object (self)
   val _name:string = name
-  val hp = 100
-  method to_string = "Instance of class people. Name: " ^ name ^ " hp: " ^ (string_of_int hp)
+  val _hp = 100
+  method to_string = "Instance of class people. Name: " ^ name ^ " hp: " ^ (string_of_int _hp)
   method talk = print_endline ("I'm " ^ _name ^ "! Do you know the doctor?")
   method die = print_endline "Aaaarghh!"
   initializer print_endline "A new character appears!"; print_endline self#to_string
