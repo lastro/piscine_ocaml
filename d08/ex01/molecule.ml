a6faecbd5d132f3c62a927d633b7b97488f29fe4
Thomas Lepetit <thomas.lepetit1990@gmail.com>
2015-06-26 13:30:27 +0200

    maj

--- d08/ex01/molecule.ml
+++ d08/ex01/molecule.ml
@@ -6,13 +6,21 @@
 (*   By: tlepetit <thomas.lepetit1990@gmai...>                                *)
 (*                                                                            *)
 (*   Created: 2015/06/25 21:58:17 by tlepetit                                 *)
-(*   Updated: 2015/06/25 22:29:39 by tlepetit                                 *)
+(*   Updated: 2015/06/26 13:22:00 by tlepetit                                 *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 class virtual molecule (name:string) (lst:(Atom.atom list)) =
 object
   method name = name
-  method symbol:(Atom.atom list) = lst
+  method symbol:(Atom.atom list) =
+	let rec count a (list:(Atom.atom list)) =
+	  match list with
+	  |[] -> 0
+	  |hd::tl when hd#equals a -> 1 + (count a tl)
+	  |hd::tl -> count a tl
+	in
+	count (new Hydrogen.hydrogen) lst;
+	lst
 end
 		  
